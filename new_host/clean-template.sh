308031df6080b3898354abbbda5608fd208819a9
andrradar <168593294+andrradar@users.noreply.github.com>
2024-09-06 19:11:35 +0300

    Update clean-template.sh

--- new_host/clean-template.sh
+++ new_host/clean-template.sh
@@ -15,5 +15,23 @@ sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
 sudo rm -f /var/lib/dbus/machine-id
 sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
 
+# Очистка истории bash (ввода команд в терминал)
+sudo rm -f ~/.bash_history
+history -c
+
+# Очистка кэша apt
+sudo apt clean
+
+# Очистка временных файлов
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
+
+# Очистка логов системы
+sudo find /var/log -type f -exec truncate -s 0 {} \;
+
+# Очистка мусорных файлов пользователей (если применимо)
+sudo rm -rf /home/*/.cache/*
+sudo rm -rf /root/.cache/*
+
 # Рекомендуется перезагрузить систему после выполнения
 echo "Система очищена. Перезагрузите машину для завершения."
