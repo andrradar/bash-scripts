35fa9d7d0ce7062e9348723d371739e7f59678b6
andrradar <168593294+andrradar@users.noreply.github.com>
2024-09-06 18:51:07 +0300

    Create clean-template.sh

---
+++ new_host/clean-template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Удаляем machine-id и создаем новый при следующем запуске
+sudo rm -f /etc/machine-id
+sudo systemd-machine-id-setup
+
+# Удаляем старые DHCP lease
+sudo rm -f /var/lib/dhcp/dhclient.*.leases
+
+# Удаляем правила udev для сетевых интерфейсов
+sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+# Удаляем machine-id для D-Bus и создаем символическую ссылку на новый machine-id
+sudo rm -f /var/lib/dbus/machine-id
+sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
+
+# Рекомендуется перезагрузить систему после выполнения
+echo "Система очищена. Перезагрузите машину для завершения."
