4d0f603b11ed9ff46f2b6bfc508c1b46eb4570c9
andrradar <168593294+andrradar@users.noreply.github.com>
2024-09-06 19:22:03 +0300

    Update clean-template.sh

--- new_host/clean-template.sh
+++ new_host/clean-template.sh
@@ -11,6 +11,19 @@ sudo rm -f /var/lib/dhcp/dhclient.*.leases
 # Удаляем правила udev для сетевых интерфейсов
 sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
 
+# Удаляем конфигурацию Netplan (если используется)
+sudo rm -f /etc/netplan/*.yaml
+
+# Создаем новый Netplan-файл (если требуется)
+sudo tee /etc/netplan/01-netcfg.yaml <<EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: yes
+EOF
+
 # Удаляем machine-id для D-Bus и создаем символическую ссылку на новый machine-id
 sudo rm -f /var/lib/dbus/machine-id
 sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
@@ -36,6 +49,6 @@ sudo rm -rf /root/.cache/*
 # Даем системе немного времени для завершения работы команд
 sleep 5
 
-# Перезагружаем систему
-echo "Очистка завершена. Перезагружаемся..."
-sudo reboot
+# Выключаем систему
+echo "Очистка завершена. Выключаем машину для клонирования..."
+sudo poweroff
