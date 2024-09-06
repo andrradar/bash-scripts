2eb407fc41ff1286c7b2e200c533f498fdfd672c
andrradar <168593294+andrradar@users.noreply.github.com>
2024-09-06 15:32:17 +0300

    Update client.sh

--- wireguard/client.sh
+++ wireguard/client.sh
@@ -1,1 +1,16 @@
-1
+#!/bin/bash
+### Настройка ВМ
+
+sudo apt update
+sudo apt install wireguard-tools
+# Клиентский конфиг client1.conf с сервера надо сохранить на ВМ под именем wg0.conf в текущей дириктории
+sudo mv wg0.conf /etc/wireguard/
+
+# Меням порт ssh на 62222?
+# sudo sed -i 's/#Port 22/Port 62222/' /etc/ssh/sshd_config
+
+# Запускаем WG
+sudo systemctl daemon-reload
+sudo systemctl restart ssh.service
+sudo systemctl enable wg-quick@wg0.service
+sudo systemctl start wg-quick@wg0.service
