dabe7bbee09219e2888090677fd0b57c4e73b3e9
andrradar <168593294+andrradar@users.noreply.github.com>
2024-09-07 07:24:50 +0300

    Update client.sh

--- wireguard/client.sh
+++ wireguard/client.sh
@@ -1,16 +1,26 @@
-# https://raw.githubusercontent.com/andrradar/bash-scripts/main/wireguard/client.sh
 #!/bin/bash
+
 ### Настройка ВМ
 
 sudo apt update
 sudo apt install wireguard-tools
-# Клиентский конфиг client1.conf с сервера надо сохранить на ВМ под именем wg0.conf в текущей дириктории
-sudo mv wg0.conf /etc/wireguard/
 
-# Меням порт ssh на 62222?
+# Ищем файл с расширением .conf в домашней директории
+conf_file=$(find ~ -maxdepth 1 -name "*.conf" | head -n 1)
+
+if [ -z "$conf_file" ]; then
+    echo "Конфигурационный файл .conf не найден в домашней директории."
+    exit 1
+fi
+
+# Переименовываем найденный файл в wg0.conf и перемещаем его
+sudo mv "$conf_file" /etc/wireguard/wg0.conf
+echo "Конфигурационный файл $conf_file перемещён в /etc/wireguard/ под именем wg0.conf"
+
+# Меняем порт ssh на 62222 (если необходимо)
 # sudo sed -i 's/#Port 22/Port 62222/' /etc/ssh/sshd_config
 
-# Запускаем WG
+# Перезапускаем WireGuard
 sudo systemctl daemon-reload
 sudo systemctl restart ssh.service
 sudo systemctl enable wg-quick@wg0.service
